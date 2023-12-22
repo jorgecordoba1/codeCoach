6ec87e401ef8d3766db9489106024b34843ba919
jorgecordoba1 <jorgecordoba@me.com>
2023-12-22 16:53:22 -0500

    Circle nows button, and button is now par of form

---
+++ 007ageCalculator/script.js
@@ -0,0 +1,15 @@
+/*let dayContainer = document.querySelector(".dayContainer");
+let monthContainer = document.querySelector(".monthContainer");
+let yearContainer = document.querySelector(".yearContainer");
+let day = document.querySelector("#day");
+let month = document.querySelector("#month");
+let year = document.querySelector("#year");
+let circle = document.querySelector(".circle");
+
+circle.addEventListener("click", answer)
+
+function answer() {
+    dayContainer.classList.add(".error")
+} */
+
+
