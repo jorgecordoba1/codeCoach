e5f73bd328a04057fad9b54df6b2887c4f149223
jorgecordoba1 <jorgecordoba@me.com>
2023-12-26 05:24:30 -0500

    update

--- 007ageCalculator/script.js
+++ 007ageCalculator/script.js
@@ -11,5 +11,26 @@ circle.addEventListener("click", answer)
 function answer() {
     dayContainer.classList.add(".error")
 } */
+function moveRight() {
+  var button = document.getElementById('myButton');
+
+  // Obtén la posición actual
+  var currentPosition = button.getBoundingClientRect().left;
+
+  // Calcula la nueva posición
+  var newPosition = currentPosition + 200;
+
+  // Aplica la nueva posición
+  button.style.left = newPosition + 'px';
+
+  // Eliminar la clase después de un tiempo suficiente para que ocurra la transición
+  setTimeout(function() {
+    button.style.transition = 'none'; // Deshabilitar temporalmente la transición
+    button.style.left = '50%'; // Restablecer la posición a la original
+    button.offsetHeight; // Fuerza un reflow, lo que activa la transición nuevamente
+    button.style.transition = ''; // Habilitar la transición
+  }, 500); // El tiempo debe coincidir con el tiempo de la transición (0.5s)
+}
+  
 
 
