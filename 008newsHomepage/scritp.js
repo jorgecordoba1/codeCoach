76145957fabc073dcd21e122c20d7846dc904280
jorgecordoba1 <jorgecordoba@me.com>
2023-12-27 14:05:16 -0500

    menu open and close working

---
+++ 008newsHomepage/scritp.js
@@ -0,0 +1,15 @@
+let menuButton = document.querySelector(".menuButton");
+let menuPop = document.querySelector('.menuPop');
+let menuClose = document.querySelector('.menuClose');
+
+menuButton.addEventListener("click", openMenu);
+
+function openMenu() {
+    menuPop.style.display = 'block';
+}
+
+menuClose.addEventListener("click", closeMenu);
+
+function closeMenu() {
+    menuPop.style.display = '';
+}
