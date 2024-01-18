9480d8bb29f8dd639150abcaac75bb8442230608
jorgecordoba1 <jorgecordoba@me.com>
2024-01-18 06:47:30 -0500

    updated

--- 013timeTracking/script.js
+++ 013timeTracking/script.js
@@ -1,20 +1,64 @@
+/*let data;
+
 function cargarDatos() {
-    fetch('./data.json')
-      .then(response => response.json())
-      .then(data => {
-        // Trabaja con los datos aquí
-        console.log('Datos cargados:', data);
-  
-        // Ejemplo: Acceder a la información de Work
-        console.log('Title:', data[0].title);
-        console.log('Daily - Current:', data[0].timeframes.daily.current);
-        console.log('Weekly - Previous:', data[0].timeframes.weekly.previous);
-        // Puedes hacer lo mismo para los demás elementos del array data
-      })
-      .catch(error => {
-        console.error('Error al cargar los datos:', error);
-      });
-  }
-  
-  cargarDatos();
-  
+  fetch('./data.json')
+    .then(response => response.json())
+    .then(jsonData => {
+      data = jsonData;
+    })
+    .catch(error => {
+      console.error('Error al cargar los datos:', error);
+    });
+}*/
+
+let daily = document.getElementById("daily");
+let weekly = document.getElementById("weekly");
+let monthly = document.getElementById("monthly");
+
+daily.addEventListener("click", function() {
+  document.getElementById("workHrs").innerText = "5hrs";
+  document.getElementById("playHrs").innerText = "1hrs";
+  document.getElementById("studyHrs").innerText = "0hrs";
+  document.getElementById("exerciseHrs").innerText = "1hrs";
+  document.getElementById("socialHrs").innerText = "1hrs";
+  document.getElementById("selfCareHrs").innerText = "0hrs";
+  document.querySelectorAll(".period").forEach(function(element) {element.innerText = "Yesterday";})
+  document.getElementById("periodWorkHrs").innerText = "7hrs";
+  document.getElementById("periodPlayHrs").innerText = "2hrs";
+  document.getElementById("periodStudyHrs").innerText = "1hrs";
+  document.getElementById("periodExerciseHrs").innerText = "1hrs";
+  document.getElementById("periodSocialHrs").innerText = "3hrs";
+  document.getElementById("periodSelfCareHrs").innerText = "1hrs";
+})
+
+weekly.addEventListener("click", function() {
+  document.getElementById("workHrs").innerText = "32hrs";
+  document.getElementById("playHrs").innerText = "10hrs";
+  document.getElementById("studyHrs").innerText = "4hrs";
+  document.getElementById("exerciseHrs").innerText = "4hrs";
+  document.getElementById("socialHrs").innerText = "5hrs";
+  document.getElementById("selfCareHrs").innerText = "2hrs";
+  document.querySelectorAll(".period").forEach(function(element) {element.innerText = "Last Week";})
+  document.getElementById("periodWorkHrs").innerText = "36hrs";
+  document.getElementById("periodPlayHrs").innerText = "8hrs";
+  document.getElementById("periodStudyHrs").innerText = "7hrs";
+  document.getElementById("periodExerciseHrs").innerText = "5hrs";
+  document.getElementById("periodSocialHrs").innerText = "10hrs";
+  document.getElementById("periodSelfCareHrs").innerText = "2hrs";
+})
+
+monthly.addEventListener("click", function() {
+  document.getElementById("workHrs").innerText = "103hrs";
+  document.getElementById("playHrs").innerText = "23hrs";
+  document.getElementById("studyHrs").innerText = "13hrs";
+  document.getElementById("exerciseHrs").innerText = "11hrs";
+  document.getElementById("socialHrs").innerText = "21hrs";
+  document.getElementById("selfCareHrs").innerText = "7hrs";
+  document.querySelectorAll(".period").forEach(function(element) {element.innerText = "Last Month";})
+  document.getElementById("periodWorkHrs").innerText = "128hrs";
+  document.getElementById("periodPlayHrs").innerText = "29hrs";
+  document.getElementById("periodStudyHrs").innerText = "19hrs";
+  document.getElementById("periodExerciseHrs").innerText = "18hrs";
+  document.getElementById("periodSocialHrs").innerText = "23hrs";
+  document.getElementById("periodSelfCareHrs").innerText = "11hrs";
+})
